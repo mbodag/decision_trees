a4e1eac13105964fe92245079f0afd983fe6398e
Matis Bodaghi <matisbodaghi@gmail.com>
2023-10-31 15:08:09 +0000

    Refactored main.py and separated eval code and pruning code

--- main.py
+++ main.py
@@ -4,41 +4,49 @@ import numpy as np
 import json
 from numpy.random import default_rng
 #TODO Clean up this file
-def main():
-    seed = default_rng(444233429)
+def main(seed):
+    
     #Load data
     clean_data = np.loadtxt('wifi_db/clean_dataset.txt')
     noisy_data = np.loadtxt('wifi_db/noisy_dataset.txt')
 
-    #Split the dataset
+    eval_cross_validation(clean_data, seed)
+    eval_cross_validation(noisy_data, seed)
+
+def eval_cross_validation(data, seed): 
+    
     evaluation_vector = np.zeros(10)
+    confusion_matrix = np.zeros((4,4))
     for i in range(10):
-        train_data, test_data = evaluate.split_dataset(clean_data, 0.1, seed)
+        #Split the dataset
+        train_data, test_data = evaluate.split_dataset(data, 0.1, seed)
 
         #First cross-validation evaluation bit
         decision_tree, depth = decision_tree_learning(train_data, 0)
         evaluation_vector[i] = evaluate.evaluate(test_data, decision_tree)
+        confusion_matrix += evaluate.confusion_matrix(test_data[:,:-1], test_data[:,-1], decision_tree)
+    
+    #For the evaluation section
+    average_eval = np.average(evaluation_vector)
+    average_confusion_matrix = confusion_matrix / 10
+    print(average_confusion_matrix, average_eval)
+    #file = open('example_tree.json','w')
+    #json.dump(decision_tree, file, indent = 4)
+
+
+def pruning_tests(data, seed):
+    for i in range(10):
+        #Split the dataset
+        train_data, test_data = evaluate.split_dataset(data, 0.1, seed)
         
         for j in range(10):
             #Pruning cross-validation bit
-            cross_train_data, validation_data = evaluate.split_dataset(train_data, 0.9, seed) #
-            #I realised I split the train_data and put the training bit back into train_data which exponentially decreased the amount of training data on every loop. 
-            #Took me an hour to figure out what was wrong, I thought it was my decision tree algorithm. 
-            #Therefore I'm renaming the variable to cross_train_data, but feel free to change it
+            cross_train_data, validation_data = evaluate.split_dataset(train_data, 0.9, seed)
             decision_tree, depth = decision_tree_learning(cross_train_data, 0)
-    
-    #For the evaluation section
-    average_first_eval = np.average(evaluation_vector)
-
-    #file = open('example_tree.json','w')
-    #json.dump(decision_tree, file, indent = 4)
-    conf_matrix = evaluate.confusion_matrix(test_data[:,:-1], test_data[:,-1], decision_tree)
-    print(conf_matrix)
-    evaluate.print_metrics(conf_matrix)
-
 
 if __name__ == '__main__':
-    main()
+    seed = default_rng(444233429)
+    main(seed)
 
 
 
